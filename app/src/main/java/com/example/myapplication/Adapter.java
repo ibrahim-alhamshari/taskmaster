4a8657b71dd51244d62fe55d1ce329d4b964f3fb
ibrahim-alhamshari <ibrahimalhamshari742@gmail.com>
2021-08-31 18:48:36 +0300

    adding some description

--- app/src/main/java/com/example/myapplication/Adapter.java
+++ app/src/main/java/com/example/myapplication/Adapter.java
@@ -11,18 +11,35 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 import java.util.List;
 
+// 1- Create the class without extensions ..
+// 6- Extend RecyclerView.Adapter - StudentAdapter.StudentViewHolder
 public class Adapter extends RecyclerView.Adapter<Adapter.TaskViewHolder> {
 
+    // 2- create the list the the adapter will use to bind data
     List<TaskModel> allTasks = new ArrayList<TaskModel>();
 
     public Adapter(List<TaskModel> allTasks){
         this.allTasks=allTasks;
     }
 
+    // 3- create the ViewHolder class (Wraps the data and the view)
+    public static class TaskViewHolder extends RecyclerView.ViewHolder {
+        // 4- The model object
+        public TaskModel taskModel;
+        // 5- view object
+        View itemView;
+        // + setting the itemView value
+        public TaskViewHolder(@NonNull View itemView){
+            super(itemView);
+            this.itemView= itemView;
+
+        }
+    }
+
     @NonNull
     @Override
     public TaskViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
+        // 7- create the view
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_task, parent, false);
         TaskViewHolder taskViewHolder= new TaskViewHolder(view);
         return taskViewHolder;
@@ -46,14 +63,5 @@ public class Adapter extends RecyclerView.Adapter<Adapter.TaskViewHolder> {
         return allTasks.size();
     }
 
-    public static class TaskViewHolder extends RecyclerView.ViewHolder {
-        public TaskModel taskModel;
-        View itemView;
 
-        public TaskViewHolder(@NonNull View itemView){
-            super(itemView);
-            this.itemView= itemView;
-
-        }
-    }
 }
